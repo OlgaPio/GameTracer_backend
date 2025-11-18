4f879dfcb85f916ee9c52b881bd1b9a5dd944f32
Olga Muñoz <lm4109080@gmail.com>
2025-11-18 09:35:53 -0500

    Se agrega el controlador para operaciones de juegos

---
+++ controllers/gameControllers.js
@@ -0,0 +1,31 @@
+const Game = require('../models/Game');
+
+const getGames = async (req, res) => {
+  try {
+    const games = await Game.find();
+    res.json(games);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener juegos' });
+  }
+};
+
+const createGame = async (req, res) => {
+  try {
+    const game = new Game(req.body);
+    await game.save();
+    res.json(game);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al crear juego' });
+  }
+};
+
+const updateGame = async (req, res) => {
+  try {
+    const game = await Game.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.json(game);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al actualizar juego' });
+  }
+};
+
+module.exports = { getGames, createGame, updateGame };
