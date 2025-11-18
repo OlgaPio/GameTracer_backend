52d5fe090019f036db8f77daa2d639a3fb931248
Olga Muñoz <lm4109080@gmail.com>
2025-11-18 09:34:41 -0500

    se agrega el modelo Review.js para las reseñas

---
+++ models/Review.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  gameId: String,
+  content: String,
+  rating: Number,
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
